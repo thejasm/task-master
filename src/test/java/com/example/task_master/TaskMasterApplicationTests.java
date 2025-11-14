7065e271c94308ca9e33b09b2fa11757f4b42d6e
Thejas Menon <thejasm@gmail.com>
2025-11-14 14:34:05 +0530

    modified:   src/test/java/com/example/task_master/TaskMasterApplicationTests.java

--- src/test/java/com/example/task_master/TaskMasterApplicationTests.java
+++ src/test/java/com/example/task_master/TaskMasterApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.task_master;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.taskmaster.TaskMasterApplication;
+
+@SpringBootTest(classes = TaskMasterApplication.class)
 class TaskMasterApplicationTests {
 
 	@Test
